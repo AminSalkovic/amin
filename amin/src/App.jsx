e3ae4497e8a6c5d50315494b423ab8e8803bab49
AminSalkovic <aminsalkovic2019@gmail.com>
2022-11-09 15:59:13 +0100

    amin

--- amin/src/App.jsx
+++ amin/src/App.jsx
@@ -1,29 +1,14 @@
 import React,{useState} from 'react';
-import {Routes,Route} from 'react-router-dom'
-import './App.css';
-import Home from './components/home/Home';
-import Login from './components/login/Login';
 
 
 
 
 function App() {
-  const[email,setEmail]=useState('')
-  const[password,setPassword]=useState('')
-
+ 
   return (
     <>
-       <div>
-             {email==='amin' && password==='saki' ? <Home/> : <Login email={setEmail} password={setPassword}/>}
-              <Routes>
-                 <Route path={'/'} element={<Home/>} />
-          
-              </Routes>
-             
-       </div> 
+      <h1>some</h1>
     </>
-   
-   
   );
 }
 
--- amin/src/components/login/Login.jsx
+++ amin/src/components/login/Login.jsx
@@ -1,13 +1,16 @@
 import React from 'react'
 import './Login.css'
 const Login = ({email,password}) => {
-  
+
+   const handleSubmit=(e)=>{
+         email((previous)=>previous=e.target.value)
+   }
   return (
     <div className='login' >
-        <form > 
-          <input type="text" placeholder='email' onChange={(e)=> email((previous)=>previous=e.target.value) }/>
+        <form> 
+          <input type="text" placeholder='email' onChange={handleSubmit }/>
           <input type="password" placeholder='password' onChange={(e)=>password((previous)=>previous=e.target.value)}/>
-          <button type='submit'>Log in</button>       
+          <button type='submit'>Log in</button>   
         </form>
     </div>
   )
